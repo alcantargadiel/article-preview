951f875db0270c3866c56a99e18db9f8fa2a696b
Gadiel Alcántar <gadiel.ar@gmail.com>
2020-08-07 12:56:58 -0500

    Adding script to js file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
+var toggle = document.getElementById("share-btn");
+var getIconsBar = document.getElementById("social-icons-bar");
 
+toggle.addEventListener("click", function() {
+    getIconsBar.classList.toggle("appear");
+}, false);
